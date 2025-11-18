82e4a7a306cc2b5c22f59ba43667ed3b40b381f8
sa-rajput <101544549+sa-rajput@users.noreply.github.com>
2025-11-18 13:51:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -28,34 +28,29 @@ provider "google" {
 }
 
 # 2. Get cluster data to configure the Kubernetes & Helm providers
-# This allows Terraform to connect to the GKE cluster it just created.
-
 data "google_client_config" "default" {}
 
 data "google_container_cluster" "protecto_data" {
-  # This dynamically gets the cluster info created in main.tf
-  # We use a "data" source to avoid a circular dependency
   project  = var.project_id
   name     = google_container_cluster.protecto.name
   location = var.region
-
-  # This explicit dependency ensures the cluster is
-  # fully created *before* these providers try to connect.
   depends_on = [
     google_container_cluster.protecto
   ]
 }
 
-# 3. Configure the Helm provider (FIXED SYNTAX)
-# The "kubernetes" block is removed, and attributes are top-level.
+# 3. Configure the Helm provider (CORRECTED)
+# Helm requires connection details to be nested in a 'kubernetes' block.
 provider "helm" {
-  host                   = "https://${data.google_container_cluster.protecto_data.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.protecto_data.master_auth[0].cluster_ca_certificate)
+  kubernetes {
+    host                   = "https://${data.google_container_cluster.protecto_data.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_container_cluster.protecto_data.master_auth[0].cluster_ca_certificate)
+  }
 }
 
-# 4. Configure the Kubernetes provider (FIXED SYNTAX)
-# The "kubernetes" block is removed, and attributes are top-level.
+# 4. Configure the Kubernetes provider (CORRECTED)
+# Kubernetes provider uses top-level arguments.
 provider "kubernetes" {
   host                   = "https://${data.google_container_cluster.protecto_data.endpoint}"
   token                  = data.google_client_config.default.access_token
